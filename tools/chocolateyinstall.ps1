df72918fb03919b022b545ef71d995b8ce4bb1ee
hurbeana <hurbeana@gmail.com>
2019-08-26 15:23:35 +0200

    initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,39 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://riotgamespatcher-a.akamaihd.net/releases/live/installer/deploy/League%20of%20Legends%20installer%20EUW.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'leagueoflegendsEUW*'
+
+  checksum      = '3BD13850BFD10EE8FA4A83DE3E4AAC386A88053C3EDD4BC4770D77D3DD1D3803'
+  checksumType  = 'sha256'
+
+  validExitCodes= @(0)
+  silentArgs   = '--mode unattended'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
